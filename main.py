61689674f1556db1694b0b20adb58574f3e83c80
BhaskarUprety <bhaskaruprety3@gmail.com>
2019-10-28 00:12:11 +0530

    Get live Data

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ from create import CreateNewExcelFile
 
 run_first_stage()
 
-# # To get client Data
-# client_data = get_all_client_data()
-# dictionary = convert_to_dictionary(client_data, client_data_list_format)
-# product_count = len(dictionary['products'])
+# To get client Data
+client_data = get_all_client_data()
+dictionary = convert_to_dictionary(client_data, client_data_list_format)
+product_count = len(dictionary['products'])
 sl = get_serial_no(sl_p, sl_n)
-dictionary = {'name': 'Rudransh Jagannath', 'address': 'Tihu, Nalvari, Assam', 'products': [('Bhindi', 2.5), ('Pav Bhaji', 4.0), ('chole', 7.0), ('rajma', 5.0), ('dal makhani', 4.0), ('yellow dal', 6.5)], 'advance': 0, 'discount': 0, 'notes': ['Chole is hard and oily', 'Please cook the food well', 'Only the dehydration of your food is carried out']}
 dictionary.update({'invoice no': sl})
 dictionary.update({'date': '27/07/2019'})
 
